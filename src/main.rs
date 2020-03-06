8eb9050f627edbd0701b04801806359a3735a245
jamil4321 <jamil.ahmed.ja28@gmail.com>
2020-03-06 23:05:27 +0500

    final

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use stmlib::{entry, iprint, iprintln, prelude::*};
 
 #[entry]
 fn main() -> ! {
-    let (mut delay, mut leds,gpioa, rcc, mut itm,mono_timer) = stmlib::init();
+    let (mut delay, mut leds, gpioa, rcc, mut itm, mono_timer) = stmlib::init();
     rcc.ahbenr.modify(|_, w| w.iopaen().set_bit());
     gpioa.moder.modify(|_, w| w.moder0().input());
     loop {
@@ -19,8 +19,14 @@ fn main() -> ! {
 
         let elapsed = instant.elapsed();
         let mut dist = ((elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6) / 2.0) / 29.1;
+        if dist <= 3.0 {
+            iprintln!(&mut itm.stim[0], "Out of Range");
+        } else if dist >= 400.0 {
+            iprintln!(&mut itm.stim[0], "Out of Range");
+        } else {
+            iprintln!(&mut itm.stim[0], "Distance = {} cm", dist);
+        }
 
-        iprintln!(&mut itm.stim[0], "Distance = {} cm", dist);
         delay.delay_ms(50_u16);
     }
 }
